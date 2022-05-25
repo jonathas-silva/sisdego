2652993ab555a30ff0170b71da55e51c60ed70c6
jonathas-silva <silva.jonathas@outlook.com>
2022-05-25 00:02:38 -0300

    Navbar - colapsamento do menu ao clicar em algum item (telas xs)

--- frontend/src/components/Header_logado.tsx
+++ frontend/src/components/Header_logado.tsx
@@ -1,22 +1,26 @@
-import { Col, Container, Nav, Navbar, Row, Stack } from 'react-bootstrap'
+import { useState } from 'react';
+import { Container, Nav, Navbar, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Header_logado.css'
 
+
 export default function Header() {
+    const [open, setOpen] = useState(true);
     return (
         <Stack direction='horizontal' className='principal-header align-items-center justify-content-between'>
             <Navbar expand="sm" className='principal-header'>
                 <Container className='p-0'>
-                    <Navbar.Toggle aria-controls="menu" />
-                    <Navbar.Collapse id="menu">
+                    <Navbar.Toggle aria-controls="menu" onClick={() => setOpen(!open)} />
+                    <Navbar.Collapse in={open} id="menu">
                         <Nav className='me-auto'>
-                            <Link to="/" className='nav-link'>Nova Solicitação</Link>
-                            <Link to="/historico" className='nav-link'>Histórico</Link>
-                            <Link to="/cadastro" className='nav-link'>Cadastro</Link>  
+                            <Link to="/" className='nav-link' onClick={() => setOpen(!open)}>Nova Solicitação</Link>
+                            <Link to="/historico" className='nav-link' onClick={() => setOpen(!open)}>Histórico</Link>
+                            <Link to="/cadastro" className='nav-link' onClick={() => setOpen(!open)}>Cadastro</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <button className="navbar-toggler" type="button"></button>
             <h2 className='logo m-0 p-2 d-flex'>SISDEGO</h2>
         </Stack>
     )
